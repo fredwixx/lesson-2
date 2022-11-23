c11e3c6d40ef6b758b7be8794a7ad8c394f5067c
fredwixx <abdubaeva95@mail.ru>
2022-11-23 13:20:34 +0600

    это 2 дз

---
+++ app.js
@@ -0,0 +1,17 @@
+/* Задача 1 */
+let a = 1;
+while (a < 50) {
+    alert(a);
+    a++;
+}
+
+for (let a = 1; a < 50; a++) {
+    alert(a);
+}
+
+/* Задача 2 */
+let arr = ['Привет', 'мир', '!'];
+alert(arr[0] + arr[1] + arr[2]);
+
+
+
